c0ff6a5f7ae662eafdb860c9737a757e57980365
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2024-06-08 16:41:49 +0200

    some styling fixes

---
+++ PortfolioBlazorApp/PortfolioBlazorApp/Models/SkillModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PortfolioBlazorApp.Models;
+
+public class SkillModel
+{
+    public string ImageSrc { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
+
